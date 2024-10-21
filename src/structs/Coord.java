2f5d51300338dfd63304ded419a3d1d888beefcd
DorianREYNIER <dorian.reynier@etu.univ-cotedazur.fr>
2024-10-21 14:51:03 +0200

    class coord no test

--- src/structs/Coord.java
+++ src/structs/Coord.java
@@ -1,9 +1,42 @@
-class Coords{
-    int x;
-    int y;
+package src.structs;
 
-    public Coords(int x, int y){
+import static java.lang.Math.*;
+
+class Coord {
+    private int x;
+    private int y;
+
+    public Coord(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return "<" + x + ", " + y + ">";
+    }
+
+    public boolean equals(Coord other) {
+        return x == other.getX() && y == other.getY();
+    }
+
+    public Coord add(Coord other) {
+        return new Coord(x + other.getX(), y + other.getY());
+    }
+
+    public Coord subtract(Coord other) {
+        return new Coord(x - other.getX(), y - other.getY());
+    }
+
+    public double distance(Coord other) {
+        return sqrt(Math.pow((other.getX() - x), 2) + Math.pow((other.getY() - y), 2));
     }
 }
